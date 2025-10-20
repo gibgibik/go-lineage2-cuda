eb6debd0cfbc2d5eabe8005127021f520dcd18a6
Your Name <you@example.com>
2025-10-20 23:08:46 +0300

    paddleocr on gpu

--- internal/ocr/target.go
+++ internal/ocr/target.go
@@ -13,7 +13,7 @@ type Target struct {
 
 func NewTarget(logger *zap.SugaredLogger) *Target {
 	p, err := paddleocr.NewPpocr("/opt/paddleocr/bin/PaddleOCR-json",
-		paddleocr.OcrArgs{}, "-models_path", "/opt/paddleocr/models", "-rec_char_dict_path", "/opt/paddleocr/models/dict_en.txt", "-rec_model_dir", "/opt/paddleocr/models/en_PP-OCRv3_rec_infer")
+		paddleocr.OcrArgs{}, "-use_gpu", "-models_path", "/opt/paddleocr/models", "-rec_char_dict_path", "/opt/paddleocr/models/dict_en.txt", "-rec_model_dir", "/opt/paddleocr/models/en_PP-OCRv3_rec_infer")
 	if err != nil {
 		panic(err)
 	}
